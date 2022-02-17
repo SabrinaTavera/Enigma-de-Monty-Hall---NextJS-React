797d8fc9944bf6eaf7c25f7e940a3a0e670ab950
SabrinaTavera <sabrina.tavera@hotmail.com>
2022-02-17 13:58:20 -0300

    LAYOUT tela inicial

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,33 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { useState } from 'react'
-import Porta from '../components/Porta'
-import Presente from '../components/Presente'
-import PortaModel from '../model/porta'
-import { atualizarPortas, criarPortas } from "../functions/portas"
+// import Head from 'next/head'
+// import Image from 'next/image'
+// import { useState } from 'react'
+// import Porta from '../components/Porta'
+// import Presente from '../components/Presente'
+// import PortaModel from '../model/porta'
+// import { atualizarPortas, criarPortas } from "../functions/portas"
+import Cartao from '../components/Cartao'
+import Link from 'next/link'
+import styles from "../styles/Formulario.module.css"
 
 // import styles from '../styles/Home.module.css'
 
-export default function Form() {
+export default function Formulario() {
 
   return (
-      <div style={{display: "flex"}}>
-        <h1>Formulário de inicio</h1>
+      <div className={styles.formulario}>
+        <div>
+          <Cartao bgcolor="#c0392c">
+            <h1>Molt Hall</h1>
+          </Cartao>
+          <Cartao bgcolor="#fff"></Cartao>
+        </div>
+        <div>
+          <Cartao></Cartao>
+          <Cartao bgcolor="#28a085">
+            <Link href={`/jogo/4/2`}><h2 className={styles.link}>Iniciar</h2></Link>
+            
+          </Cartao>
+        </div>
         
       </div>
   )
